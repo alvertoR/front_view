53c06916e46f1cfd339c3c46fa349e6398d7f462
Alberto Ayala Rodriguez <alberto.ayala@newshore.es>
2022-03-08 22:24:25 -0500

    base architecture and other things

---
+++ src/app/log-out/log-out.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LogOutRoutingModule } from './log-out-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LogOutRoutingModule
+  ]
+})
+export class LogOutModule { }
